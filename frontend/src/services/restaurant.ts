7414beea83022d9e9778488512552ba8456a4df6
Unkzmc296 <41935630v@gmail.com>
2025-12-12 17:04:22 +0800

    t串接後端使用者個人排隊資訊-modify

--- frontend/src/services/restaurant.ts
+++ frontend/src/services/restaurant.ts
@@ -5,6 +5,7 @@ import type {
   UserQueueStatusResponse,
 } from '@/types/RestaurantApi'
 
+// 餐廳資訊
 export async function getRestaurants(): Promise<RestaurantListResponse> {
   const response = await fetch(`http://localhost:8000/api/restaurants`)
   if (!response.ok) {
@@ -13,6 +14,7 @@ export async function getRestaurants(): Promise<RestaurantListResponse> {
   return await response.json()
 }
 
+// 在 home page 點我要排隊時出現的餐廳排隊資訊
 export async function getQueueStatus(restaurantId: number): Promise<QueueStatusResponse> {
   const response = await fetch(`http://localhost:8000/api/restaurants/${restaurantId}/queue/status`)
   if (!response.ok) {
@@ -21,7 +23,8 @@ export async function getQueueStatus(restaurantId: number): Promise<QueueStatusR
   return await response.json()
 }
 
-export async function getUserQueueStatus(restaurantId: number): Promise<QueueStatusResponse> {
+// 個人的排隊狀態
+export async function getUserQueueStatus(restaurantId: number): Promise<UserQueueStatusResponse> {
   const response = await fetch(`http://localhost:8000/api/user/{user_id}/queue`)
   if (!response.ok) {
     throw new Error('Network response was not ok')
